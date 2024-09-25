ee67de7a81239f51320567de5c5f596a81070f80
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-25 10:31:51 +0800

    update:  added pixel post request

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from config import *
 import requests as rq
-
+from datetime import datetime as dt
 USERNAME = "ejarao"
 
 pixela_endpoint = "https://pixe.la/v1/users"
@@ -16,7 +16,7 @@ user_params = {
 # print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-
+graph_header = {"X-USER-TOKEN": pixela_token}
 graph_params = {
     "id": "graph1",
     "name": "cycling",
@@ -25,4 +25,19 @@ graph_params = {
     "color": "sora"
 }
 
-response = rq.post(url=graph_endpoint, json=graph_params)
+# response = rq.post(url=graph_endpoint, json=graph_params, headers=graph_header)
+# print(response.text)
+
+user_response = input("Write 'y' to post a pixel to your endpoint: ")
+
+if user_response == "y":
+
+    pixel_params = {
+        "date": dt.now().strftime("%Y%m%d"),
+        "quantity": "9.43",
+    }
+    post_pixel = rq.post(url=f"{graph_endpoint}/{graph_params['id']}", headers=graph_header, json=pixel_params)
+    print(pixel_params["date"])
+    print(post_pixel.text)
+else:
+    print("No input detected")
